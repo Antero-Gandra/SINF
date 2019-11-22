71e41be27b7c42be8f0836dc13a952c60f3ef61d
Bruno Carvalho <elbrunovsky@gmail.com>
2019-11-22 17:33:03 +0000

    Move views and add App.js

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
+import { createBrowserHistory } from "history";
+
+import Admin from "layouts/Admin.js";
+
+// template css, keep this
+import "assets/css/material-dashboard-react.css?v=1.8.0";
+
+const hist = createBrowserHistory();
+
+export default function App() {
+  return (
+    <Router history={hist}>
+      <Switch>
+        <Route path="/admin" component={Admin} />
+        {/* <Route path="/login" component={Login} /> */}
+        <Redirect from="/" to="/admin/template" />
+      </Switch>
+    </Router>
+  );
+}
--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,3 @@
-/*!
-
-=========================================================
-* Material Dashboard React - v1.8.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/material-dashboard-react
-* Copyright 2019 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/material-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 // @material-ui/icons
 import Dashboard from "@material-ui/icons/Dashboard";
 import Person from "@material-ui/icons/Person";
@@ -22,18 +5,17 @@ import LibraryBooks from "@material-ui/icons/LibraryBooks";
 import BubbleChart from "@material-ui/icons/BubbleChart";
 import Notifications from "@material-ui/icons/Notifications";
 // core components/views for Admin layout
-import DashboardPage from "views/Dashboard/Dashboard.js";
-import UserProfile from "views/UserProfile/UserProfile.js";
-import TableList from "views/TableList/TableList.js";
-import Typography from "views/Typography/Typography.js";
-import Icons from "views/Icons/Icons.js";
-import NotificationsPage from "views/Notifications/Notifications.js";
+import DashboardPage from "views/creativetim/Dashboard/Dashboard.js";
+import UserProfile from "views/creativetim/UserProfile/UserProfile.js";
+import TableList from "views/creativetim/TableList/TableList.js";
+import Typography from "views/creativetim/Typography/Typography.js";
+import Icons from "views/creativetim/Icons/Icons.js";
+import NotificationsPage from "views/creativetim/Notifications/Notifications.js";
 
 const dashboardRoutes = [
   {
-    path: "/dashboard",
-    name: "Dashboard",
-    rtlName: "لوحة القيادة",
+    path: "/template",
+    name: "Template Dashboard",
     icon: Dashboard,
     component: DashboardPage,
     layout: "/admin"
@@ -41,7 +23,6 @@ const dashboardRoutes = [
   {
     path: "/user",
     name: "User Profile",
-    rtlName: "ملف تعريفي للمستخدم",
     icon: Person,
     component: UserProfile,
     layout: "/admin"
@@ -49,7 +30,6 @@ const dashboardRoutes = [
   {
     path: "/table",
     name: "Table List",
-    rtlName: "قائمة الجدول",
     icon: "content_paste",
     component: TableList,
     layout: "/admin"
@@ -57,7 +37,6 @@ const dashboardRoutes = [
   {
     path: "/typography",
     name: "Typography",
-    rtlName: "طباعة",
     icon: LibraryBooks,
     component: Typography,
     layout: "/admin"
@@ -65,7 +44,6 @@ const dashboardRoutes = [
   {
     path: "/icons",
     name: "Icons",
-    rtlName: "الرموز",
     icon: BubbleChart,
     component: Icons,
     layout: "/admin"
@@ -73,7 +51,6 @@ const dashboardRoutes = [
   {
     path: "/notifications",
     name: "Notifications",
-    rtlName: "إخطارات",
     icon: Notifications,
     component: NotificationsPage,
     layout: "/admin"
