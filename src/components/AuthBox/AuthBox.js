ebf6a37830dd429a70bacbbd4309bf6a29074968
up201605769 <up201605769@fe.up.pt>
2019-12-02 21:19:21 +0000

    Removed warnings and added frontend functionality to components
    Still need to implement several API calls

--- src/components/AuthBox/AuthBox.js
+++ src/components/AuthBox/AuthBox.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
@@ -13,32 +13,111 @@ import Button from '@material-ui/core/Button';
 const useStyles = makeStyles(styles);
 
 export default function AuthBox() {
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [APIkey, setAPIkey] = useState("");
+  const [usernameErrors, setUsernameErrors] = useState(false);
+  const [passwordErrors, setPasswordErrors] = useState(false);
+  const [APIkeyErrors, setAPIkeyErrors] = useState(false);
+
+
+  const checkUsername = () => {
+    let isValid = true;
+    // API call to check GET username
+    return isValid;
+  }
+
+  const checkPassword = () => {
+    let isValid = true;
+    // API call to check GET password
+    return isValid;
+  }
+
+  const checkAPIkey = () => {
+    let isValid = false;
+    // API call to check GET API key
+    return isValid;
+  }
+
+  const handleValidation = () => {
+    let isValid = true;
+
+    if(!checkUsername()){
+      setUsernameErrors(true);
+      isValid = false;
+    }
+
+    if(!checkPassword()){
+      setPasswordErrors(true);
+      isValid = false;
+    }
+
+    if(!checkAPIkey()){
+      setAPIkeyErrors(true);
+      isValid = false;
+    }
+
+    return isValid;
+  }
+
+  const handleSubmit = (event) => {
+    if (event) {
+      event.preventDefault();
+    }
+
+    if(handleValidation()){
+      callback();
+    }
+
+    else {
+      alert("Invalid Jasmin authentication.");
+    }
+  }
+
   const classes = useStyles();
+
+  const callback = () => {
+    alert(`User synced!
+           Name: ${username}
+           Key: ${APIkey}`);
+  }
+
+  const errorStyle = {
+    color: 'red',
+    fontSize: '13px',
+  };
+
   return (
-    <Table className={classes.table}>
-      <TableBody>
-          <TableRow key={1}>
-            <TableCell>
-              <TextField label="Enter your username" />
-            </TableCell>
-          </TableRow>
+    <form onSubmit={handleSubmit}>
+      <Table className={classes.table}>
+        <TableBody>
+            <TableRow key={1}>
+              <TableCell>
+                <TextField required value={username} onChange={e => setUsername(e.target.value)} label="Enter your username" />
+                {usernameErrors && <p style={errorStyle}>Invalid username</p>}
+              </TableCell>
+            </TableRow>
 
-          <TableRow key={2}>
-            <TableCell>
-              <TextField label="Enter your password" />
-            </TableCell>
-          </TableRow>
+            <TableRow key={2}>
+              <TableCell>
+                <TextField type="password" required value={password} onChange={e => setPassword(e.target.value)} label="Enter your password" />
+                {passwordErrors && <p style={errorStyle}>Invalid password</p>}
+              </TableCell>
+            </TableRow>
 
-          <TableRow key={3}>
-            <TableCell> 
-              <TextField label="Enter your API key" />
-            </TableCell>
+            <TableRow key={3}>
+              <TableCell> 
+                <TextField required value={APIkey} onChange={e => setAPIkey(e.target.value)} label="Enter your API key" />
+                {APIkeyErrors && <p style={errorStyle}>Invalid API key</p>}
+              </TableCell>
 
-            <TableCell> 
-              <Button variant="contained">Submit</Button>
-            </TableCell>
-          </TableRow>
-      </TableBody>
-    </Table>
+              <TableCell> 
+                <Button type="submit" variant="contained">Submit</Button>
+              </TableCell>
+            </TableRow>
+        </TableBody>
+      </Table>
+    </form>
   );
 }
--- src/components/SubscriptionBox/SubscriptionBox.js
+++ src/components/SubscriptionBox/SubscriptionBox.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
@@ -19,66 +19,128 @@ function createData(supplier, purchaseOrders, date, unsubscribe){
   return {supplier, purchaseOrders, date, unsubscribe};
 }
 
-const rows = [
-  createData('S1', 0, "12/2/2019", false),
-  createData('S2', 0, "12/2/2019", false),
-  createData('S3', 0, "12/2/2019", false),
-  createData('S4', 0, "12/2/2019", false),
-  createData('S5', 0, "12/2/2019", false)
-];
-
-export default function AuthBox() {
+export default function SubscriptionBox() {
   const classes = useStyles();
+
+  const errorStyle = {
+    color: 'red',
+    fontSize: '13px',
+  };
+
+  const [APIkey, setAPIkey] = useState("");
+  const [APIkeyErrors, setAPIkeyErrors] = useState(false);
+
+  const checkAPIkey = () => {
+    let isValid = true;
+    // API call to check GET API key
+    return isValid;
+  }
+
+  // will have to fetch values from the database instead of having these hardcoded here ..
+  // GET API call of subscribers
+  const [rows, setRows] = useState([
+    createData('S1', 0, "12/2/2019", false),
+    createData('S2', 0, "12/2/2019", false),
+    createData('S3', 0, "12/2/2019", false),
+    createData('S4', 0, "12/2/2019", false),
+    createData('S5', 0, "12/2/2019", false)
+  ]);
+
+  const unsubscribeFrontEnd = (supplier) => {
+    setRows(rows.filter(item => item.supplier !== supplier));
+    let removeIndex = rows.map(function(item) { return item.supplier; }).indexOf(supplier);
+    rows.splice(removeIndex, 1);
+  }
+
+  const unsubscribeBackEnd = (supplier) => {
+    // DELETE API call to unsubscribe
+  }
+
+  const unsubscribe = (supplier) => {
+    unsubscribeFrontEnd(supplier);
+    unsubscribeBackEnd(supplier);
+  }
+
+  const handleValidation = () => {
+    let isValid = true;
+
+    if(!checkAPIkey()){
+      setAPIkeyErrors(true);
+      isValid = false;
+    }
+
+    return isValid;
+  }
+
+  const handleSubmit = (event) => {
+    if (event) {
+      event.preventDefault();
+    }
+
+    if(handleValidation()){
+      callback();
+    }
+
+    else {
+      alert("Invalid Jasmin authentication.");
+    }
+  }
+
+  const callback = () => {
+    alert(`Subscribed!
+           Key: ${APIkey}`);
+    // API call to SUBSCRIBE (POST?)
+  }
+
   return (
-    <Table>
-      <TableHead>
-        <TableRow>
-          <TableCell align="center">Supplier</TableCell>
-          <TableCell align="center">#Pending Purchase Orders</TableCell>
-          <TableCell align="center">Date</TableCell>
-          <TableCell align="center">Unsubscribe</TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {rows.map(row => (
-          <TableRow key={row.supplier}>
-            <TableCell align="center" component="th" scope="row">
-              {row.supplier}
-            </TableCell>
-            <TableCell align="center">{row.purchaseOrders}</TableCell>
-            <TableCell align="center">{row.date}</TableCell>
-            <TableCell align="center"><IconButton aria-label="Close" className={classes.tableActionButton}>
-              <Close className={classes.tableActionButtonIcon + " " + classes.close}/>
-              </IconButton>
+    <form onSubmit={handleSubmit}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell align="center">Supplier</TableCell>
+            <TableCell align="center">#Pending Purchase Orders</TableCell>
+            <TableCell align="center">Date</TableCell>
+            <TableCell align="center">Unsubscribe</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map(row => (
+            <TableRow key={row.supplier}>
+              <TableCell align="center" component="th" scope="row">
+                {row.supplier}
+              </TableCell>
+              <TableCell align="center">{row.purchaseOrders}</TableCell>
+              <TableCell align="center">{row.date}</TableCell>
+              <TableCell align="center">
+                <IconButton onClick={() => unsubscribe(row.supplier)} aria-label="Close" className={classes.tableActionButton}>
+                  <Close className={classes.tableActionButtonIcon + " " + classes.close}/>
+                </IconButton>
+              </TableCell>
+            </TableRow>
+          ))} 
+        </TableBody>
+        <TableBody>
+          <TableRow>
+            <TableCell>
+              <TextField 
+              rows="4" 
+              style={{width: 200}} 
+              multiline 
+              variant="outlined"
+              required
+              onChange={e => setAPIkey(e.target.value)} 
+              label="Insert Jasmin API key to subscribe" 
+              />
+              {APIkeyErrors && <p style={errorStyle}>Invalid API key</p>}
             </TableCell>
           </TableRow>
-        ))} 
-      </TableBody>
-
-      <div>
-        <br></br>
-
-        <div>
-          <div>
-            <TextField 
-            rows="4" 
-            fullwidth  
-            style={{width: 200}} 
-            multiline 
-            variant="outlined" 
-            label="Insert Jasmin API key to subscribe" 
-            />
-          </div>
-
-          <br></br>
-
-          <div>
-            <Button id="test" variant="contained">Submit</Button>
-          </div>
-         
-        </div>
-      </div>
-      
-    </Table>
+          <TableRow>
+            <TableCell>
+              <Button type="submit" variant="contained">Submit</Button>
+            </TableCell>
+          </TableRow>
+        </TableBody>      
+      </Table>
+    </form>
   );
 }
