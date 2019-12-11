5dbf340ce390470d8d800bc5ce7566ca5a1d83bf
up201605769 <up201605769@fe.up.pt>
2019-12-11 16:06:12 +0000

    Removed validation from authentication
    There is no true way to authenticate due to the way primavera works

--- src/components/AuthBox/AuthBox.js
+++ src/components/AuthBox/AuthBox.js
@@ -17,64 +17,13 @@ export default function AuthBox() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [APIkey, setAPIkey] = useState("");
-  const [usernameErrors, setUsernameErrors] = useState(false);
-  const [passwordErrors, setPasswordErrors] = useState(false);
-  const [APIkeyErrors, setAPIkeyErrors] = useState(false);
-
-
-  const checkUsername = () => {
-    let isValid = true;
-    // API call to check GET username
-    return isValid;
-  }
-
-  const checkPassword = () => {
-    let isValid = true;
-    // API call to check GET password
-    return isValid;
-  }
-
-  const checkAPIkey = () => {
-    let isValid = false;
-    // API call to check GET API key
-    return isValid;
-  }
-
-  const handleValidation = () => {
-    let isValid = true;
-
-    if(!checkUsername()){
-      setUsernameErrors(true);
-      isValid = false;
-    }
-
-    if(!checkPassword()){
-      setPasswordErrors(true);
-      isValid = false;
-    }
-
-    if(!checkAPIkey()){
-      setAPIkeyErrors(true);
-      isValid = false;
-    }
-
-    return isValid;
-  }
 
   const handleSubmit = (event) => {
     if (event) {
       event.preventDefault();
     }
 
-    if(handleValidation()){
-      callback();
-    }
-
-    else {
-      alert("Invalid Jasmin authentication.");
-    }
-
-    
+    callback();
   }
 
   const classes = useStyles();
@@ -85,11 +34,6 @@ export default function AuthBox() {
            Key: ${APIkey}`);
   }
 
-  const errorStyle = {
-    color: 'red',
-    fontSize: '13px',
-  };
-
   return (
     <form onSubmit={handleSubmit}>
       <Table className={classes.table}>
@@ -97,21 +41,18 @@ export default function AuthBox() {
             <TableRow key={1}>
               <TableCell>
                 <TextField required value={username} onChange={e => setUsername(e.target.value)} label="Enter your username" />
-                {usernameErrors && <p style={errorStyle}>Invalid username</p>}
               </TableCell>
             </TableRow>
 
             <TableRow key={2}>
               <TableCell>
                 <TextField type="password" required value={password} onChange={e => setPassword(e.target.value)} label="Enter your password" />
-                {passwordErrors && <p style={errorStyle}>Invalid password</p>}
               </TableCell>
             </TableRow>
 
             <TableRow key={3}>
               <TableCell> 
                 <TextField required value={APIkey} onChange={e => setAPIkey(e.target.value)} label="Enter your API key" />
-                {APIkeyErrors && <p style={errorStyle}>Invalid API key</p>}
               </TableCell>
 
               <TableCell> 
